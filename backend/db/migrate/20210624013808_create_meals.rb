bd776f121353ec3d94ecc7760d2e79e881346fac
adriana <picadoadriana7@gmail.com>
2021-06-23 21:45:28 -0400

    set relationships

---
+++ backend/db/migrate/20210624013808_create_meals.rb
@@ -0,0 +1,12 @@
+class CreateMeals < ActiveRecord::Migration[6.1]
+  def change
+    create_table :meals do |t|
+      t.string :name
+      t.string :ingredients
+      t.integer :calories
+      t.string :category_id
+
+      t.timestamps
+    end
+  end
+end
