6b7f186d998cc3cd71d632df58c99214708eb4b6
adriana <picadoadriana7@gmail.com>
2021-06-23 22:44:36 -0400

    add seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Fast = Category.create(name: 'fast')
+Sweet = Category.create(name: 'sweet')
+Sour = Category.create(name: 'sour')
+
+
+Meal.create([
+    {name: "Burger", ingredients: "bun, patty, tomato, lettuce", calories: "450", category: "Fast"},
+    {name: "Candy", ingredients: "sugar", calories: "290", category: "Sweet"},
+    {name: "Vinegar", ingredients: "vinegar ing", calories: "30", category: "Sour"},
+)]
