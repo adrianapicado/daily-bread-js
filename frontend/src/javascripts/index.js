e25074b3e5994b383066172f4598ebdc92066428
adriana <picadoadriana7@gmail.com>
2021-06-23 22:48:30 -0400

    update javascripts

--- frontend/src/javascripts/index.js
+++ frontend/src/javascripts/index.js
@@ -2,18 +2,23 @@ const apiService = new ApiService()
 
 let main = document.getElementById('main')
 
-window.addEventListener("DOMContentLoaded", () => {
-    getCategories()
-})
-
-function getCategories() {
-    let main = document.getElementById('main')
-    fetch(base_url + '/categories')
-    .then(res => res.json())
-    .then(categories => {
-        console.log(categories)
-    })
+const init = () => {
+    bindEventListeners()
+    renderCategories() 
 }
+
+// window.addEventListener("DOMContentLoaded", () => {
+//     getCategories()
+// })
+
+// function getCategories() {
+//     let main = document.getElementById('main')
+//     fetch(base_url + '/categories')
+//     .then(res => res.json())
+//     .then(categories => {
+//         console.log(categories)
+//     })
+// }
 
 function bindClicksToLinks() {
     const categories = document.querySelectorAll("li a")
