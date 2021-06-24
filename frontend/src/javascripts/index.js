d915699dd9d34c905d38500760cde2be2e1a474e
adriana <picadoadriana7@gmail.com>
2021-06-24 00:30:50 -0400

    add function

--- frontend/src/javascripts/index.js
+++ frontend/src/javascripts/index.js
@@ -11,3 +11,13 @@ function bindEventListeners() {
     document.getElementById('category-form').addEventListener('click', displayCreateForm)
     document.getElementById('categories').addEventListener('click', renderCategories)
 }
+
+async function renderCategories() {
+    const categories = await apiService.fetchCategories()
+    main.innerHTML = ""
+    categories.map(category => {
+        const newCategory = new Category(category)
+        main.innerHTML += newCategory.render()
+    })
+    bindClicksToLinks()
+}
