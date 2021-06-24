1dab8acfb52963b45627fa2b607c94471575813d
adriana <picadoadriana7@gmail.com>
2021-06-23 22:05:43 -0400

    add bind click function

--- frontend/src/javascripts/index.js
+++ frontend/src/javascripts/index.js
@@ -3,11 +3,19 @@ const base_url = 'http://localhost:3000'
 window.addEventListener("DOMContentLoaded", () => {
     getCategories()
 })
+
 function getCategories() {
     let main = document.getElementById('main')
     fetch(base_url + '/categories')
     .then(res => res.json())
     .then(categories => {
         console.log(categories)
+    })
+}
+
+function bindClicksToLinks() {
+    const categories = document.querySelectorAll("li a")
+    categories.forEach(category => {
+        category.addEventListener('click', displayCategory)
     })
 }
