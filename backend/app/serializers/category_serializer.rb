327635f6fdc953b9982d16cab9234936c7c162d7
adriana <picadoadriana7@gmail.com>
2021-06-23 21:47:23 -0400

    set serializers

--- backend/app/serializers/category_serializer.rb
+++ backend/app/serializers/category_serializer.rb
@@ -1,3 +1,5 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :ingredients, :calories
+  belongs_to :category
+
 end
--- backend/app/serializers/food_serializer.rb
+++ backend/app/serializers/food_serializer.rb
@@ -1,3 +1,4 @@
 class FoodSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name 
+  has_many :foods
 end
