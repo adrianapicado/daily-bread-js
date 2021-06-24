f6cbb2673dff7efca71934eccd04f0f5acff3256
adriana <picadoadriana7@gmail.com>
2021-06-24 00:36:27 -0400

    add function

--- frontend/src/javascripts/index.js
+++ frontend/src/javascripts/index.js
@@ -122,17 +122,11 @@ async function displayMeal(e) {
     document.getElementById('delete-meal').addEventListener('click', removeMeal) 
 }
 
-async function displayCategory(id){
-    const data = await apiService.fetchCategory(id)
-    const category = new Category(data)
-    main.innerHTML = category.renderCategory()
-    if (category.meals) {
-        category.meals.forEach(meal => {
-            main.innerHTML += `
-            <li><a href="#" data-id="${meal.id}" data-category-id="${category.id}">${meal.name}</a> </li>
-            <br>
-            `
-        })
+function mealsInCategory() {
+    document.querySelectorAll("li a").length
+    
+}
+
 
 
 
