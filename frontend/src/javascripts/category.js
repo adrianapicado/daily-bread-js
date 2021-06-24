115171de2291ee7f0e6a1ca3cf43fa4bc4ca500c
adriana <picadoadriana7@gmail.com>
2021-06-23 22:34:50 -0400

    create class category

---
+++ frontend/src/javascripts/category.js
@@ -0,0 +1,28 @@
+class Category {
+  constructor(data){
+    this.id = data.id
+    this.name = data.name
+    this.meals = data.meals
+    }
+
+    render() {
+        return `
+        <li>
+            <a href="#" data-id="${this.id}">${this.name}</a>
+        </li>
+        ` 
+    }
+
+    renderCategory() {
+        return `
+            <h3>${this.name}</h3>
+            <button id="add-song" data-id="${this.id}">Add Meal</button>
+            <hr>
+            
+            <div id="add-meal-div">
+            </div>
+            <br>
+            <br>
+            `
+    }
+}
