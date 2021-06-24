a1fa19873ae7fc2c3c16702636ab0f4e1d6977be
adriana <picadoadriana7@gmail.com>
2021-06-23 22:53:28 -0400

    add async categories

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -2,4 +2,18 @@ class ApiService {
     constructor() {
         this.base_url = 'http://localhost:3000'
     } 
+
+    async fetchCategories() {
+        let res = await fetch(this.base_url + '/categories')
+        let data = await res.json()
+        return data
+    }
+
+    async fetchCategory(id) {
+        let res = await fetch(this.base_url + `/categories/${id}`)
+        let data = await res.json()
+        return data
+    }
+
+
 }
